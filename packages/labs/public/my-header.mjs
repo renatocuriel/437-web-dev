739d2209fa20b5466f9ac62c35afaae00e8ee2dd
Diego Curiel <diegocuriel@Diegos-MacBook-Pro-88.local>
2025-02-01 16:21:13 -0800

    header shit

--- packages/labs/public/my-header.mjs
+++ packages/labs/public/my-header.mjs
@@ -2,7 +2,16 @@ import { attachShadow } from "./utils.mjs";
 
 const TEMPLATE = document.createElement("template");
 TEMPLATE.innerHTML = `
-    Auto-rendered as a child of the custom element
+    <header class="navbar">
+        <h1>Diego Curiel</h1>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="projects.html">Projects</a></li>
+                <li><a href="favoritefoods.html">Favorite Foods</a></li>
+            </ul>
+        </nav>
+    </header>
 `;
 
 class myHeader extends HTMLElement {
