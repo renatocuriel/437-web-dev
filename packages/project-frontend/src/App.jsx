92f46f26de42f6ee5e2e8388b22a361df0c7013a
Diego Curiel <diegocuriel@Mac-345.lan>
2025-02-15 22:29:07 -0800

    fixed the darkmode toggle for project

--- packages/project-frontend/src/App.jsx
+++ packages/project-frontend/src/App.jsx
@@ -10,10 +10,7 @@ import "./index.css";
 function App() {
   return (
     <Router>
-    <div className="flex min-h-screen bg-red-100 dark:bg-blue-500">
-      If this is not blue in dark mode, dark mode is  broken.
-    </div>
-      <div className="flex min-h-screen bg-red-500 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
+      <div className="flex min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100">
         <Sidebar />
         <div className="flex-1">
           <Navbar />
--- packages/project-frontend/src/components/DarkModeToggle.jsx
+++ packages/project-frontend/src/components/DarkModeToggle.jsx
@@ -11,7 +11,6 @@ const DarkModeToggle = () => {
   return (
     <button
       onClick={toggleDarkMode}
-    //   className="relative px-4 py-2 rounded bg-gray-200 dark:bg-gray-700"
       className="px-4 py-2 rounded-lg bg-gray-300 dark:bg-gray-700 text-gray-800 dark:text-white shadow-md hover:bg-gray-400 dark:hover:bg-gray-600 transition"
     >
       {darkMode ? "Light Mode" : "Dark Mode"}
